beae4764f91b6a69e843cd414678ffa327949e12
osarobo <osarobo@github.com>
2025-10-18 18:54:16 -0400

    Added client and user api routes

--- src/main/java/org/coms4156/tars/RouteController.java
+++ src/main/java/org/coms4156/tars/RouteController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +16,29 @@ public class RouteController {
     return "Welcome to the TARS Home Page!";
   }
 
+
+  /**
+   * Create new client route
+   * @param clientId
+   *
+   * @return
+   */
+  @PostMapping("/api/v1/clients/{clientId}")
+  public ResponseEntity<String> createClientRoute(@PathVariable String clientId) {
+    // Logic to create a client would go here.
+    return new ResponseEntity<>("Client route created for clientId: " + clientId, HttpStatus.CREATED);
+  }
+  
+  /**
+   * Handles POST requests to create a user.
+   * @param clientId The ID of the client.
+   * @param userId The ID of the user.
+   *
+   * @return A ResponseEntity indicating the result of the operation.
+  */
+  @PostMapping("/api/v1/clients/{clientId}/users/{userId}")
+  public ResponseEntity<String> createClientUserRoute(@PathVariable String userId) {
+    // Logic to create a user would go here.
+    return new ResponseEntity<>("User route created for userId: " + userId, HttpStatus.CREATED);
+  }
 }
