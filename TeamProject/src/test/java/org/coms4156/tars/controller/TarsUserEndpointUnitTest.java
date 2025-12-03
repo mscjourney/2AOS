c0c933820363cc370789029cb2a857f259565a60
Anya Khatri <anyakhatri@example.com>
2025-12-03 18:32:17 +0000

    Fix checkstyle warnings in TarsUserEndpointUnitTest and ClientEndPointUnitTest

--- TeamProject/src/test/java/org/coms4156/tars/controller/TarsUserEndpointUnitTest.java
+++ TeamProject/src/test/java/org/coms4156/tars/controller/TarsUserEndpointUnitTest.java
@@ -3,22 +3,22 @@ package org.coms4156.tars.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.coms4156.tars.model.TarsUser;
-import org.coms4156.tars.service.TarsUserService;
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.coms4156.tars.model.TarsUser;
+import org.coms4156.tars.service.TarsUserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 /**
  * Endpoint tests for TarsUser related GET endpoints.
