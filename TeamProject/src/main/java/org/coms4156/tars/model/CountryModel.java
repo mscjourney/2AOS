11b7689472c6d860eb13c10de2dae3fc7e7650ba
SeungHyun Sung <sshmagic099@gmail.com>
2025-12-02 22:50:08 -0500

    removed unused getAdvisoryForCountry and its tests

--- TeamProject/src/main/java/org/coms4156/tars/model/CountryModel.java
+++ TeamProject/src/main/java/org/coms4156/tars/model/CountryModel.java
@@ -44,23 +44,6 @@ public class CountryModel {
     }
   }
 
-
-  /**
-   * Returns the advisory JSON node for a given country.
-   */
-  public JsonNode getCountryInfo(String country) {
-    if (country == null || country.trim().isEmpty()) {
-      throw new IllegalArgumentException("Country cannot be empty.");
-    }
-
-    for (JsonNode node : advisoryArray) {
-      if (node.get("country").asText().equalsIgnoreCase(country)) {
-        return node;
-      }
-    }
-    return null;
-  }
-
   /**
    * Returns a TravelAdvisory object for a given country.
    */
